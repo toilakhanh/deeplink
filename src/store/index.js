ce588455e86d0263dc728f2729923d33f838cb6c
khanh.nguyen2 <khanh.nguyen2@m-os6-khanhnd2.ntq-solution.com.vn>
2023-02-21 19:23:14 +0700

    update reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
-
+import {counterSlice} from '../reducer';
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counterSlice,
+  },
 });
